95ead41c53bfdd8b9fd4b1b0618699029b89f416
Jayden1411 <66936265+Jayden1411@users.noreply.github.com>
2022-12-06 04:12:25 +0200

    Update vacancies.php

--- vacancies.php
+++ vacancies.php
@@ -25,7 +25,7 @@
 					    <h5 class="card-title"><a href="#"class="click-call"data-id="<?php echo $vacancies[$x]->company_ref; ?>"><?php echo ucwords($vacancies[$x]->company_ref); ?></a></h5>
 					    <h6 class="card-title">Ref:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $vacancies[$x]->vacancy_ref ?></h6>
 					    <h6 class="card-title">Title:&nbsp;&nbsp;&nbsp;&nbsp;
-					    <a href="https://webapp.placementpartner.com/wi/application_form.php?id=<?php echo$vacancies[$x]->company_ref ?>&vacancy_ref=<?php echo $vacancies[$x]->vacancy_ref ?>&source=assessment"target="blank"data-id="<?php echo $vacancies[$x]->vacancy_ref; ?>"class="click-call">
+					    <a href="#"data-id="<?php echo $vacancies[$x]->vacancy_ref; ?>"class="click-call">
 					    <?php echo $vacancies[$x]->job_title;  ?></a></h6>
 					    <h6 class="card-title">Expiry:&nbsp;&nbsp;<?php echo $vacancies[$x]->expiry_date;  ?></h6>
 					    <p class="card-text"><?php echo html_entity_decode($vacancies[$x]->brief_description ) ?>
